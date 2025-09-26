92a16ec6351a5967ab84df00fdb010034d7608c3
williamgomez686 <williamgomez686@gmail.com>
2025-09-26 17:08:51 -0600

    Creacion del controlador para el ejercicio con los filtros para el select de categorias y los producots

--- PruebaTecnica2025/Controllers/VentasController.cs
+++ PruebaTecnica2025/Controllers/VentasController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PruebaTecnica2025.Data;
+using PruebaTecnica2025.Models;
 using PruebaTecnica2025.Models.ViewModels;
 
 namespace PruebaTecnica2025.Controllers
@@ -32,9 +34,38 @@ namespace PruebaTecnica2025.Controllers
         }
 
         [HttpGet]
-        public async Task<IActionResult> Ejercicio()
+        public async Task<IActionResult> Ejercicio(int CodCategoria, int anio)
         {
-            _con
+            int anioFiltro = (anio>0)? anio : 2019;
+
+            var categoriasConVentas = await _context.Categoria
+                .Where(c => c.Productos.Any(p => p.Venta.Any(v => v.Fecha.HasValue && v.Fecha.Value.Year == anioFiltro)))
+                .OrderBy(c => c.Nombre)
+                .ToListAsync();
+
+            ViewBag.ListaCategorias = new SelectList(categoriasConVentas, "CodigoCategoria", "Nombre");
+            ViewBag.AnioSeleccionado = anioFiltro;
+
+            var resutlado = await _context.Venta
+                                        .Where(v=>v.CodigoProductoNavigation.CodigoCategoria == CodCategoria)
+                                        .OrderBy(v=>v.Fecha)
+                                        .Select(s => new VentasVM
+                                        {
+                                            CodigoVenta = s.CodigoVenta,
+                                            FechaVenta = s.Fecha,
+                                            NombreProducto = s.CodigoProductoNavigation.Nombre,
+                                            NombreCategoria = s.CodigoProductoNavigation.CodigoCategoriaNavigation.Nombre
+                                        })
+                                        .ToListAsync();
+
+            return View (resutlado);    
+        }
+        [HttpGet]
+        public async Task<IActionResult> ListaCategorias(int id)
+        {
+            var venta = await _context.Categoria.Where(c=>c.CodigoCategoria == id).OrderBy(o=>o.Nombre).ToArrayAsync();
+            
+            return View(venta);
         }
     }
 }
